5cb3f8194655cfc71d22c4710823c81944a42c3b
Tilanka_Mihingu <tilankamihingu2018@gmail.com>
2021-04-02 12:50:20 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,100 @@
 
 import './App.css';
+import Header from './components/Header';
+import Item from './components/Item';
+
+import Accessories from './assets/Desktop-Accessories.jpg';
+import ModelS from './assets/Desktop-ModelS.jpeg';
+import Model3 from './assets/Desktop-Model3.jpeg';
+import ModelX from './assets/Desktop-ModelX.jpeg';
+import ModelY from './assets/Desktop-ModelY.jpeg';
+import SolarPanels from './assets/Desktop-SolarPanels.jpeg';
+import SolarRoof from './assets/Desktop-SolarRoof.jpeg';
 
 function App() {
   return (
     <div className="App">
-     <h1>Tilanka mihingu</h1>
+     <Header />
+     <div className="app__itemsContainer">
+       <Item
+          title = 'Lowest Cost Solar Panels in America'
+          desc = 'Money back gurantee'
+          descLink = ''
+          backgroundImg = {SolarPanels}
+          leftBtnTxt = 'ORDER NOW'
+          leftBtnLink = ''
+          rightBtnTxt = 'LEARN MORE'
+          rightBtnLink = ''
+          twoButtons = 'true'
+          first 
+        />
+        <Item
+          title = 'Model S'
+          desc = '$69,420'
+          descLink = ''
+          backgroundImg = {ModelS}
+          leftBtnTxt = 'CUSTOM ORDER'
+          leftBtnLink = ''
+          rightBtnTxt = 'LEARN MORE'
+          rightBtnLink = ''
+          twoButtons = 'true'
+        />
+        <Item
+          title = 'MODEL 3'
+          desc = 'Money back gurantee'
+          descLink = ''
+          backgroundImg = {Model3}
+          leftBtnTxt = 'CUSTOM ORDER'
+          leftBtnLink = ''
+          rightBtnTxt = 'LEARN MORE'
+          rightBtnLink = ''
+          twoButtons = 'true'
+        />
+        <Item
+          title = 'MODEL X'
+          desc = 'Money back gurantee'
+          descLink = ''
+          backgroundImg = {ModelX}
+          leftBtnTxt = 'CUSTOM ORDER'
+          leftBtnLink = ''
+          rightBtnTxt = 'LEARN MORE'
+          rightBtnLink = ''
+          twoButtons = 'true'
+        />
+         <Item
+          title = 'MODEL Y'
+          desc = 'Money back gurantee'
+          descLink = ''
+          backgroundImg = {ModelY}
+          leftBtnTxt = 'CUSTOM ORDER'
+          leftBtnLink = ''
+          rightBtnTxt = 'LEARN MORE'
+          rightBtnLink = ''
+          twoButtons = 'true'
+        />
+         <Item
+          title = 'Solar for New Roofs'
+          desc = 'Money back gurantee'
+          descLink = ''
+          backgroundImg = {SolarRoof}
+          leftBtnTxt = 'ORDER NOW'
+          leftBtnLink = ''
+          rightBtnTxt = 'LEARN MORE'
+          rightBtnLink = ''
+          twoButtons = 'true'
+        />
+         <Item
+          title = 'Accessories'
+          desc = 'Money back gurantee'
+          descLink = ''
+          backgroundImg = {Accessories}
+          leftBtnTxt = 'SHOP NOW'
+          leftBtnLink = ''
+          rightBtnTxt = 'LEARN MORE'
+          rightBtnLink = ''
+         /* twoButtons = 'true'*/
+        />
+     </div>
     </div>
   );
 }
